d2c0465bc26f705421005bf215d97733d388c4ef
Heindrich Himmler Macalinao <heindrich.macalinao@gmail.com>
2026-04-25 01:26:26 +0800

    add new class

---
+++ IntroToJava/Statements.java
@@ -0,0 +1,10 @@
+package IntroToJava;
+
+public class Statements {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning Java is fun!");
+    }
+}
